a7639c89c3442a24cdfdf498d09e7bc3382f90b3
chetanmunuswamy <munuswamy.chetan@gmail.com>
2016-06-07 12:46:39 -0400

    initial commit

--- TaskTracker_jQuery/script.js
+++
@@ -1,8 +0,0 @@
-$("#submit").click(function(){
-    var name = $("#nameId").val();
-    var date = $("#dateId").val();
-    var assigned = $("#assignedId").val();
-    var list = "<tr><td>" + name + "</td><td>" + date + "</td><td>" + assigned + "</td></tr>"; 
-    var show = $("#show");
-    show.prepend(list);
-});
